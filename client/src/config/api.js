3bf45cde4a944ee68be3fc3025c645d8b4165502
adhxabre <abeldustin06@gmail.com>
2023-04-12 11:34:20 +0700

    Gomail

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Create base URL API
 export const API = axios.create({
-  baseURL: 'http://localhost:5000/api/v1/',
+  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/api/v1/",
 });
 
 // Set Authorization Token Header
 export const setAuthToken = (token) => {
   if (token) {
-    API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   } else {
-    delete API.defaults.headers.common['Authorization'];
+    delete API.defaults.headers.common["Authorization"];
   }
 };
